0b5cbb3b7b15aa57b8831672a9c5ae6c4a603c97
Natalia <alimadeoliveiranatalia@gmail.com>
2022-08-15 06:49:12 -0300

    feat: add form modal styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement("#root");
@@ -13,19 +14,14 @@ export function App() {
         setIsNewTransactionModalOpen(true);
     }
 
-    function handleCloseNewTransaction(){
+    function handleCloseNewTransactionModal(){
         setIsNewTransactionModalOpen(false);
     }
   return (
     <>
       <Header onOpenNewTransactionModal={ handleOpenNewTransactionModal } />
       <Dashboard />
-      <Modal 
-        isOpen={ isNewTransactionModalOpen }
-        onRequestClose={ handleCloseNewTransaction }    
-      >
-        <h2>Cadastrar Transacao</h2>
-      </Modal>
+      <NewTransactionModal isOpen={ isNewTransactionModalOpen } onRequestClose={ handleCloseNewTransactionModal }/>
       <GlobalStyle />
     </>
   );
